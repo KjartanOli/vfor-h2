4c6f54b8571665c486f0d7d2e041ce26ccb70893
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-04-16 14:23:50 +0000

    Add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "../ui/forms/LoginForm";
+
+export default function Page() {
+  return (<main>
+            <h1>Log In</h1>
+            <LoginForm></LoginForm>
+          </main>)
+}
