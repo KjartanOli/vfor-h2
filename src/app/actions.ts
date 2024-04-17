5c8fe95ce246ef3d09a02c13f375cbc270ebc7b6
Kjartan Oli Agustsson <koa20@hi.is>
2024-04-17 21:35:55 +0000

    Move login action into a separate actions.ts file
    Apparently something causes LoginForm to be a client component.  As
    such the server action needs to be moved to a separate file.

---
+++ src/app/actions.ts
@@ -0,0 +1,23 @@
+'use server'
+
+import { post } from "@/lib/api";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function login(data: FormData) {
+  const username = data.get('username')
+  const password = data.get('password')
+
+  const response = await post('login', { username, password });
+
+  if (!response.ok)
+    throw new Error('Network error')
+
+  const token = (await response.json()).token;
+
+  if (!token)
+    throw new Error('Error');
+
+  cookies().set('SESSION', token)
+  redirect('/');
+}
