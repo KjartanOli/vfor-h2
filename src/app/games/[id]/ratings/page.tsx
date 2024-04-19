7fcdffc04c6024b6b202090fa59cc7afb93af50f
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-04-19 19:59:03 +0000

    Implementing css for ratings

--- src/app/games/[id]/ratings/page.tsx
+++ src/app/games/[id]/ratings/page.tsx
@@ -1,4 +1,5 @@
 import { getGame, getRatings } from "@/lib/games";
+import styles from "./page.module.css";
 
 export default async function Page({ params }: { params: { id: string }}) {
   const [ratings, game] = await Promise.all([
@@ -7,9 +8,9 @@ export default async function Page({ params }: { params: { id: string }}) {
   ]);
 
   return (
-    <main>
-      <h1>Ratings for {game.name}</h1>
-      <ul>
+    <main className={styles.container}>
+      <h1 className={styles.header}>Ratings for {game.name}</h1>
+      <ul className={styles.ratings}>
         {ratings.map(r => <li key={r.user_id}>{r.rating}</li>)}
       </ul>
     </main>
