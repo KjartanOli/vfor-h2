6eb84080daee380a6803279dcd314315b73177c5
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-04-19 14:06:03 +0000

    Implementing css for Registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,11 @@
 import RegistrationForm from "../ui/forms/RegistrationForm";
+import styles from "./page.module.css";
 
 export default function Page() {
-  return (<main>
-            <h1>Register</h1>
-            <RegistrationForm></RegistrationForm>
+  return (<main className={styles.container}>
+            <h1 className={styles.header}>Register</h1>
+            <div className={styles.RegistrationFormContainer}>
+              <RegistrationForm></RegistrationForm>
+            </div>
           </main>)
 }
