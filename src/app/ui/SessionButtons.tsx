547bf756f31435f07485b0a29aae3a276066a036
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-04-19 14:04:04 +0000

    Implementing css for header

--- src/app/ui/SessionButtons.tsx
+++ src/app/ui/SessionButtons.tsx
@@ -1,12 +1,13 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { signout } from "../actions";
+import styles from "./SessionButtons.module.css";
 
 export default function SessionButtons() {
   if (cookies().get('SESSION'))
     return (<form action={signout}><button>Sign out</button></form>)
-  return (<div>
-            <Link href="/login">Log in</Link>
-            <Link href="/register">Register</Link>
+  return (<div className={styles.container}>
+            <Link href="/login" className={styles.login}>Log in</Link>
+            <Link href="/register" className={styles.register}>Register</Link>
           </div>)
 }
