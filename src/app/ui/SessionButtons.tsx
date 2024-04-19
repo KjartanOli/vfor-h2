dd2c44f9221bde905ebc088d69b5bb510ba7bf6f
Kjartan Oli Agustsson <koa20@hi.is>
2024-04-19 21:56:12 +0000

    Use get_user_id instead of manually checking for the SESSION cookie

--- src/app/ui/SessionButtons.tsx
+++ src/app/ui/SessionButtons.tsx
@@ -2,9 +2,10 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { signout } from "../actions";
 import styles from "./SessionButtons.module.css";
+import { get_user_id } from "@/lib/utils";
 
 export default function SessionButtons() {
-  if (cookies().get('SESSION'))
+  if (get_user_id() != null)
     return (<form action={signout} className={styles.container}><button className={styles.signOut}>Sign out</button></form>)
   return (<div className={styles.container}>
             <Link href="/login" className={styles.login}>Log in</Link>
