24d4a6b7e051c470dfda571d05088ba5d21410c7
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-04-19 14:05:32 +0000

    Implementing css for Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,11 @@
 import { LoginForm } from "../ui/forms/LoginForm";
+import styles from "./page.module.css";
 
 export default function Page() {
-  return (<main>
-            <h1>Log In</h1>
-            <LoginForm></LoginForm>
+  return (<main className={styles.container}>
+            <h1 className={styles.header}>Log In</h1>
+            <div className={styles.LoginFormContainer}>
+              <LoginForm></LoginForm>
+            </div>
           </main>)
 }
--- src/app/ui/forms/LoginForm.tsx
+++ src/app/ui/forms/LoginForm.tsx
@@ -1,11 +1,12 @@
 import { login } from "@/app/actions";
+import styles from "./LoginForm.module.css";
 
 export function LoginForm() {
   return (
-    <form action={login}>
-      <input type="text" name="username" placeholder="Username" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <button type="submit">Login</button>
+    <form action={login} className={styles.formContainer}>
+      <input type="text" name="username" placeholder="Username" required className={styles.username} />
+      <input type="password" name="password" placeholder="Password" required className={styles.password} />
+      <button type="submit" className={styles.button}>Login</button>
     </form>
   )
 }
